b09c32dba876da503e5282da7aa1955533a99a50
Sawyer Crosby <crosbysawyer@Sawyers-MacBook-Pro.local>
2023-09-07 18:02:18 -0700

    adding example, generalizing, tweaking templates

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# tarot_py
+# How to use this...
+1. Clone this repo.
+2. Open terminal.
+3. Activate a conda environment. 
+4. Navigate to the directory of the repo (tarot_py). 
+5. Open google chrome.
+6. Run `python tarot.py -s {spread_type}` where `{spread_type}` is either *"one", "three", "bridge", or "celtic"*. 
+7. Additional, optional arguments include:
+    - `-q {question}` where `{question}` is a string that becomes part of the seed to inform the card-selection randomizer. 
+    - `-f {filepath}` where `{filepath}` is the path to a .txt file that includes a list of cards. This argument is used if the user wishes to transcribe a spread that was drawn elsewhere. 
+      - If using `{filepath}`, the following conditions must be met:
+        - The number of cards in the .txt file must match the number of cards required in the spread. 
+        - Each card must appear on its own line (with no extra spaces at the end or beginning of the line). 
+        - Each card must be named exactly the same as the .png file in the cards/ folder -- e.g. the Fool is "majors_0."
+        - Each card must be preceded with a + or - (+ indicates upright, and - indicates reversed).
+      - See `example_filepath_for_celtic_transcription.txt` for an example of how to structure this file. 
+8. Open the html, then print -> save as PDF :)
+
+Examples of use:
+  - `python tarot.py -s three` = "draw a three card spread"
+  - `python tarot.py -s one -q "what is goin on?"` = "draw a one-card spread, and use the text after -q as part of the seed for the randomizer."
+  - `python tarot.py -s celtic -f ~/tarot_py/example_filepath_for_celtic_transcription.txt` = "create a celtic cross spread using the cards listed in `example_filepath_for_celtic_transcription` to populate the reading. 
+
+
--- tarot.py
+++ tarot.py
@@ -1,3 +1,4 @@
+## load packages
 import os
 import glob
 import datetime
@@ -6,13 +7,14 @@ from functools import reduce
 import argparse
 import webbrowser
 
-## constants
-DIR = "/Users/crosbysawyer/tarot_py/"
+## UPDATE ME IF YOU WANT ------------------------------
+dir_to_save_spreads = os.path.expanduser("~/Desktop/")
+## ----------------------------------------------------
 
-## functions
-def draw_cards(n_cards, seed, root_dir = DIR):
+## Function to draw N cards
+def draw_cards(n_cards, seed = None):
     ## list files
-    cards = [os.path.basename(x) for x in glob.glob(root_dir + "/cards/*")]
+    cards = [os.path.basename(x) for x in glob.glob(os.path.dirname(os.path.abspath(__file__)) + "/cards/*")]
     cards = [x.removesuffix(".jpg") for x in cards]
     ## set seed
     if seed == None: ## use time
@@ -26,7 +28,8 @@ def draw_cards(n_cards, seed, root_dir = DIR):
     draw = reduce(lambda res, l: res + [l[0] + l[1]], zip(directions, cards), [])
     return draw
 
-def draw_spread(spread_type, seed, card_list):
+## Function to convert N cards to a spread (either drawn or inputted)
+def draw_spread(spread_type, seed = None, card_list = None):
     ## draw cards set categories
     if spread_type == "celtic":
         if card_list == None:
@@ -34,12 +37,12 @@ def draw_spread(spread_type, seed, card_list):
         else:
             cards = card_list
         categories = ["center", "cross", "above", "below", "from", "toward", "self", "environment", "hopesfears", "advice"]
-    elif spread_type == "bridge":
+    elif spread_type == "cross":
         if card_list == None:
             cards = draw_cards(5, seed)
         else:
             cards = card_list
-        categories = ["from", "toward", "bridge", "water", "star"]
+        categories = ["top", "bottom", "left", "right", "center"]
     elif spread_type == "three":
         if card_list == None:
             cards = draw_cards(3, seed)
@@ -53,58 +56,59 @@ def draw_spread(spread_type, seed, card_list):
     spread = dict(map(lambda i,j : (i,j), categories, cards))
     return spread
 
-def save_html(spread_type, seed, card_list = None, root_dir = DIR):
+def save_html(spread_type, seed = None, card_list = None, out_dir = dir_to_save_spreads):
     ## draw spread
     spread = draw_spread(spread_type, seed, card_list)
     ## open template
-    with open(root_dir + '/templates/' + spread_type + '_template.html', 'r') as file:
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/templates/' + spread_type + '_template.html', 'r') as file:
         filedata = file.read()
     # Replace strings in HTML code
     for category in spread:
         find_1 = category + "_card"
-        replace_1 = root_dir + "/cards/" + spread[category][1:] + ".jpg"
+        replace_1 = os.path.dirname(os.path.abspath(__file__)) + "/cards/" + spread[category][1:] + ".jpg"
         find_2 = category + "_rotate"
         if spread[category][0] == "-":
-            if category in ["cross", "bridge", "water"]:
+            if category in ["cross"]:
                 replace_2 = "-90deg"
             else:
                 replace_2 = "180deg"
         elif spread[category][0] == "+":
-            if category in ["cross", "bridge", "water"]:
+            if category in ["cross"]:
                 replace_2 = "90deg"
             else:
                 replace_2 = "0deg"
         filedata = filedata.replace(find_1, replace_1)
         filedata = filedata.replace(find_2, replace_2)
     # Write a new file out
-    out_file = os.path.expanduser("~/Desktop/tarot_") + spread_type + "_" + str(datetime.datetime.now().strftime("%Y-%d-%m_%Hh%Mm")) + '.html'
+    out_file = out_dir + "/tarot_" + spread_type + "_" + str(datetime.datetime.now().strftime("%Y-%d-%m_%Hh%Mm")) + '.html'
     with open(out_file, 'w') as file:
         file.write(filedata)
     browser = webbrowser.get('chrome')
     browser.open("file://" + out_file, new = 2)
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description='Draw tarot spreads :)')
-    parser.add_argument('-s','--spread', help='What type of spread?', choices = ["celtic", "bridge", "three", "one"], required=True)
+    parser.add_argument('-s','--spread', help='What type of spread?', choices = ["celtic", "cross", "three", "one"], required=True)
     parser.add_argument('-q','--question', help='Question (seed for random draw).', required=False, default = None)
-    parser.add_argument('-f','--filepath', help='If already draw cards, can write line by line in text file and include here', required=False, default = None)
+    parser.add_argument('-f','--filepath', help='.txt file containing list of pre-drawn cards', required=False, default = None)
+    ## this text file should look the output of draw_cards() where each item is on a new line
+    ## an example is contained in this repo as ./example_filepath.txt
     args = vars(parser.parse_args())
     S = args["spread"]
     Q = args["question"]
     F = args["filepath"]
 
     if F == None: ## draw 
-        save_html(S, Q)
+        save_html(spread_type = S, seed = Q)
     else: ## read cards
         assert S != "one" ## no reason to import spread for 1 card
         with open(F) as f:
             cards = f.read().splitlines() 
         if S == "celtic":
             assert len(cards) == 10
-        if S == "bridge":
+        if S == "cross":
             assert len(cards) == 5
         if S == "three":
             assert len(cards) == 3
-        save_html(S, Q, cards)
+        save_html(spread_type = S, seed = Q, card_list = cards)
         
